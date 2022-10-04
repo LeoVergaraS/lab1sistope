653c51b4bd16a90b7e008c7dd21575fb852ac397
LeoVergaraS <leo.vergara@usach.cl>
2022-10-03 23:08:29 -0300

    [Leo Vergara]-Comentarios en el hijo.c y padre.c

--- hijo.c
+++ hijo.c
@@ -1,41 +1,49 @@
 #include "hijo.h"
 
 //Lee el archivo intermedio y crea una lista de juegos por cada hijo
-//Entrada: comienzo y fin del año
+//Entrada: comienzo, fin del año y puntero al tamaño de la lista
 //Salida: lista de juegos
 juego *listaDeJuegosHijo(int comienzo, int fin, int *i)
 {
+    // Se abre el archivo escrito por el padre.
     FILE *archivo = fopen("intermedio.txt", "r");
+    // Se posiciona el puntero al comienzo del año perteneciente al proceso.
     fseek(archivo,comienzo,SEEK_SET);
     int pos = ftell(archivo);
+    // Si no existe el archivo, se retorna NULL.
     if (archivo == NULL){
         printf("No se pudo abrir el archivo");
         return NULL;
     }
     char linea[2000];
+
     //Se cuentan las lineas del archivo
     while (fgets(linea, 2000, archivo) != NULL)
     {
+        //Se cuenta cada linea
         *i = *i + 1;
+        //Se posiciona el puntero al final de cada linea.
         pos = ftell(archivo);
+        //Si el puntero llega al final, se sale del ciclo.
         if(pos == fin){
             break;
         }
         
     }
+
+    //Se crea un arreglo de juegos con la cantidad de lineas
+    //contadas anteriormente.
     juego *lista = (juego *)malloc(sizeof(juego) * (*i));
+    //Se posiciona el puntero al comienzo del año perteneciente al proceso.
     fseek(archivo,comienzo,SEEK_SET);
     pos = ftell(archivo);
     //Se lee el archivo y se guarda en el arreglo de estructuras
     int j = 0;
-    fgets(linea, 2000, archivo);
-    ////////////////////////////////////////////////////////////////////// //printf("%s",linea);
-    fseek(archivo,comienzo,SEEK_SET);
     while (j < *i)
     {
+        //Se lee cada linea del archivo
         fgets(linea, 2000, archivo);
-        //tokenize linea
-
+        //tokenizamos la linea
         char *token = strtok(linea, ";");
         lista[j].id = atoi(token);
         token = strtok(NULL, ";");
@@ -58,47 +66,54 @@ juego *listaDeJuegosHijo(int comienzo, int fin, int *i)
         lista[j].lin = atoi(token);
         j++;
     }
-    //printf("aqui: %d  %s  %d  %f  %d  %d  %d  %d  %d  %d\n", lista[0].id, lista[0].nombre, lista[0].restriccion, lista[0].precio, lista[0].proximamente, lista[0].fecha, lista[0].gratis, lista[0].win, lista[0].mac, lista[0].lin);
     pos = ftell(archivo);
+    // Se cierra el archivo.
     fclose(archivo);
     return lista;
 }
 
 //Calcula los promedios, el más caro y el más barato de los juegos por cada hijo
-//Entrada: lista de juegos, tamaño de la lista, punteros a los promedios, el más caro y el más barato
+//Entrada: lista de juegos, tamaño de la lista, punteros a los promedios, el más caro, el más barato
+//         el precio minimo y cantidad de juegos mayor al precio minimo.
 //Salida: nada
 void calculosDelHijo(juego* juegos, int n, int *c, int *b, float *prom, float precioMinimo, int contador){
+    //Se inicializan los precios caros, baratos y una sumatoria para el promedio
     int caro = juegos[0].precio;
     int barato = juegos[0].precio;
     float suma = 0;
     for(int i=0 ;i<n;i++){
+        //Se verfica el precio más caro
         if(juegos[i].precio > caro && juegos[i].precio >= precioMinimo){
             caro = juegos[i].precio;
             *c = i;
         }
+        //Se verifica el precio más barato
         else if(juegos[i].precio < barato && juegos[i].precio >= precioMinimo){
             barato = juegos[i].precio;
             *b = i;
         }
+        //Se suma el precio de cada juego mayor al precio minimo para calcular el promedio
         if(juegos[i].precio >= precioMinimo){
             suma = suma + juegos[i].precio;
         } 
     }
+    //Se calcula el promedio
     *prom = suma/contador;
     return ;
 
 }
 
 //Calcula la cobertura de cada plataforma por año
-//Entrada: lista de juegos, tamaño de la lista, punteros a los promedios, el más caro y el más barato
+//Entrada: lista de juegos, tamaño de la lista, punteros a los porcentaje de cada plataforma,
+//         el precio minimo y cantidad de juegos mayor al precio minimo.
 //Salida: nada
 void promedioPorPlataforma(juego* juegos, int n, int *w, int *m, int *l, float precioMinimo, int contador){
-    //printf("%d %d\n",juegos[1].fecha,juegos[1].win);
+    //Se inicializan los contadores de cada plataforma
     int win = 0;
     int mac = 0;
     int lin = 0;
     for(int i=0 ;i<n;i++){
-        //printf("%d %d\n",juegos[i].fecha,juegos[i].win);
+        //Se verifica si el juego tiene la plataforma y si es mayor al precio minimo
         if(juegos[i].win == 1 && juegos[i].precio >= precioMinimo){
             win++;
         }
@@ -109,16 +124,21 @@ void promedioPorPlataforma(juego* juegos, int n, int *w, int *m, int *l, float p
             lin++;
         }
     }
+    //Se calcula el porcentaje de cada plataforma
     *w = (win/contador)*100;
     *m = (mac/contador)*100;
     *l = (lin/contador)*100;
-    //sprintf(stringFinal, "Windows: %f%% Mac: %f%% Linux: %f%%\n", w, m, l);
     return;
 }
 
+//Calcula la cantidad de juegos con precio mayores al precio minimo por año
+//Entrada: lista de juegos, tamaño de la lista, y precio minimo
+//Salida: cantidad de juegos mayores al precio minimo
 int contadorJuegosPrecioMinimo(juego* juegos, int n, int precioMinimo){
+    //Se inicializa el contador
     int contador = 0;
     for(int i=0;i<n;i++){
+        //Se verifica si el precio del juego es mayor al precio minimo
         if(juegos[i].precio >= precioMinimo){
             contador++;
         }  
--- padre.c
+++ padre.c
@@ -9,16 +9,17 @@ juego *nuevoJuego()
     return lista;
 }
 
-// Función que transforma un string a un entero 0 o 1
+// Función que transforma un string Yes, No, True o False a un entero 0 o 1
 // Entrada: string
 // Salida: entero
 int transformarStringABool(char *cadena)
 {
-    // Si 
+    // Si es Yes o True, retorna 1
     if (strcmp(cadena, "Yes") == 0 || strcmp(cadena, "True") == 0)
     {
         return 1;
     }
+    // Si es No o False, retorna 0
     else 
     {
         return 0;
@@ -27,11 +28,13 @@ int transformarStringABool(char *cadena)
 }
 
 // Función que lee el archivo y lo guarda en un arreglo de estructuras
-// Entrada: Nombre del archivo
+// Entrada: Nombre del archivo y puntero al tamaño de la lista;
 // Salida: Arreglo de estructuras juegos
 juego *listaDeJuegos(char *nombreArchivo, int *i)
 {
+    // Se abre el archivo.
     FILE *archivo = fopen(nombreArchivo, "r");
+    // Si no existe el archivo, se retorna NULL.
     if (archivo == NULL)
     {
         printf("No se pudo abrir el archivo");
@@ -41,21 +44,27 @@ juego *listaDeJuegos(char *nombreArchivo, int *i)
     //Se cuentan las lineas del archivo
     while (fgets(linea, 255, archivo) != NULL)
     {
+        //Se cuenta cada linea
         *i = *i + 1;
     }
+    //Se crea un arreglo de juegos con la cantidad de lineas
     juego *lista = (juego *)malloc(sizeof(juego) * (*i));
     char prox[255], gratis[255], lin[255], mac[255], win[255];
+
+    // Se posiciona el puntero al comienzo del archivo.
     rewind(archivo);
-    int j = 0;
-    int resto;
-    int p;
+
+    int j = 0, resto, p;
     char *token;
     char copia[255];
     //Se lee el archivo y se guarda en el arreglo de estructuras
     while (j < *i)
     {
+        // Se inicializa un contador de comas
         int contador = 0;
+        // Se lee la linea del archivo
         fgets(linea, 255, archivo);
+        // Se cuenta la cantidad de comas en la linea
         for (int k = 0; k < strlen(linea); k++)
         {
             if (linea[k] == ',')
@@ -63,6 +72,7 @@ juego *listaDeJuegos(char *nombreArchivo, int *i)
                 contador++;
             }
         }
+        // Si es 9, se copia talcual la linea
         if (contador == 9)
         {
             sscanf(linea, "%d,%[^,],%d,%f,%[^,],%d,%[^,],%[^,],%[^,],%[^,]", &lista[j].id, lista[j].nombre, &lista[j].restriccion, &lista[j].precio, prox, &lista[j].fecha, gratis, win, mac, lin);
@@ -75,18 +85,22 @@ juego *listaDeJuegos(char *nombreArchivo, int *i)
         //Se guarda en el caso de que el juego tenga una coma en el nombre
         else
         {
+            // Se maneja la lina
             p = 0;
             resto = contador - 9;
             token = strtok(linea, ",");
             while (p<9)
-            {
+            {   
+                // primer p para el id
                 if (p == 0)
                 {
                     sscanf(token, "%d", &lista[j].id);
                 }
+                // segundo p para el nombre
                 else if (p == 1)
                 {
                     strcpy(lista[j].nombre, token);
+                    // Se copia cada token segun la cantidad de comas de más
                     while (resto > 0)
                     {
                         token = strtok(NULL, ",");
@@ -96,38 +110,46 @@ juego *listaDeJuegos(char *nombreArchivo, int *i)
                         resto--;
                     }
                 }
+                // tercer p para la restriccion de edad
                 else if (p == 2)
                 {
                     sscanf(token, "%d", &lista[j].restriccion);
                 }
+                // cuarto p para el precio del juego
                 else if (p == 3)
                 {
                     sscanf(token, "%f", &lista[j].precio);
                 }
+                // quinto p para booleano de si sale proximamente
                 else if (p == 4)
                 {
                     strcpy(prox, token);
                     lista[j].proximamente = transformarStringABool(prox);
                 }
+                // sexto p para la fecha
                 else if (p == 5)
                 {
                     sscanf(token, "%d", &lista[j].fecha);
                 }
+                // septimo p para booleano de si es gratis
                 else if (p == 6)
                 {
                     strcpy(gratis, token);
                     lista[j].gratis = transformarStringABool(gratis);
                 }
+                // octavo p para booleano de si esta disponible para Windows
                 else if (p == 7)
                 {
                     strcpy(win, token);
                     lista[j].win = transformarStringABool(win);
                 }
+                // noveno p para booleano de si esta disponible para Mac
                 else if (p == 8)
                 {
                     strcpy(mac, token);
                     lista[j].mac = transformarStringABool(mac);
                 }
+                // decimo p para booleano de si esta disponible para Linux
                 else if (p == 9)
                 {
                     strcpy(lin, token);
@@ -148,39 +170,50 @@ juego *listaDeJuegos(char *nombreArchivo, int *i)
 //Salida: lista de juegos
 int* agregarCabecera(int* cabeceras, int cabecera, int *n){
     int i;
+    //Se crea un arreglo auxiliar con la cantidad de cabeceras + 1
     int* nuevasCabeceras = (int*)malloc(sizeof(int)*(*n+1));
+    //Se copian las cabeceras anteriores
     for(i=0;i<*n;i++){
         nuevasCabeceras[i] = cabeceras[i];
     }
+    //Se agrega la nueva cabecera
     nuevasCabeceras[*n] = cabecera;
     *n = *n + 1;
+    //Se libera la memoria del arreglo anterior
     free(cabeceras);
     return nuevasCabeceras;
 }
 
 // Función que escribe el juego
-// Entrada: Estructura juego
+// Entrada: nombre del archivo, lista de estructura juego, tamaño de la lista de juegos,
+//          cantidad de cabeceras
 // Salida: void
 int* escribirJuego(char *nombreSalida, juego *juego, int i,int *n)
 {
+    // Se reserva memoria a la lista de cabeceras
     int * cabeceras= (int*)malloc(sizeof(int)*(*n));
     int len;
+
+    // Se abre el archivo
     FILE *archivo= fopen(nombreSalida, "w+");
-    if (archivo == NULL)
-    {
-        printf("No se pudo abrir el archivo");
-        return NULL;
-    }
+
+    // Fecha actual para comparar
     int fechaActual = juego[0].fecha;
     
+    // Se recorre la lista de juegos
     for (int j = 0; j < i-1; j++)
     {
+            // Se escribe el juego en el archivo
             fprintf(archivo, "%d;%s;%d;%f;%d;%d;%d;%d;%d;%d\n", juego[j].id, juego[j].nombre, juego[j].restriccion, juego[j].precio, juego[j].proximamente, juego[j].fecha, juego[j].gratis, juego[j].win, juego[j].mac, juego[j].lin);
+            // Se guarda la posición de la cabecera
             len = ftell(archivo);
 
+            // Se compara la fecha del juego siguiente con la fecha actual
             if(juego[j+1].fecha != fechaActual){
+            // Se guarda la cabecera y se cambia la fecha actual
             cabeceras = agregarCabecera(cabeceras, len, &(*n));
             fechaActual = juego[j+1].fecha;
+            // Si es la ultima tupla se guarda su posicion en la cabecera
             if(j+1 == i-1){
                 fprintf(archivo, "%d;%s;%d;%f;%d;%d;%d;%d;%d;%d\n", juego[j+1].id, juego[j+1].nombre, juego[j+1].restriccion, juego[j+1].precio, juego[j+1].proximamente, juego[j+1].fecha, juego[j+1].gratis, juego[j+1].win, juego[j+1].mac, juego[j+1].lin);
                 len = ftell(archivo);
@@ -189,7 +222,7 @@ int* escribirJuego(char *nombreSalida, juego *juego, int i,int *n)
                 }
             }
     }
-        
+    // Se cierra el archivo y se retorna la lista de cabeceras 
     fclose(archivo);
     return cabeceras;
 }
@@ -199,20 +232,24 @@ int* escribirJuego(char *nombreSalida, juego *juego, int i,int *n)
 //Salida: void
 void escribirArchivoFinal(char *string,char* nombreSalida)
 {
+    //Se abre el archivo
     FILE *archivo= fopen(nombreSalida, "a");
+    //Si no se puede abrir el archivo se imprime un mensaje de error
     if (archivo == NULL)
     {
         printf("No se pudo abrir el archivo");
         return;
     }
-    fprintf(archivo,"%s\n\n",string);   
+    //Se escribe en el archivo
+    fprintf(archivo,"%s\n\n",string);  
+    //Se cierra el archivo 
     fclose(archivo);
 }
 
 
 //Esta funcion es el mergesort para ordenar por anios
-//Parametros: arreglo de enteros, arreglo de enteros, entero, entero
-//Retorno: void, se ordena por dentro
+//Entrada: arreglo de enteros, arreglo de enteros, entero, entero
+//Salida: void, se ordena por dentro
 void merge(juego* juegos,int inicio,int medio,int fin){
 	// Se inicia en cero una lista de struct de datos temporal
 	// para dejar los datos.
@@ -265,8 +302,8 @@ void merge(juego* juegos,int inicio,int medio,int fin){
 
 
 //Esta funcion es el mergesort para ordenar por anios
-//Parametros: arreglo de enteros, entero, entero
-//Retorno: void, se ordena por dentro
+//Entrada: arreglo de enteros, entero, entero
+//Salida: void, se ordena por dentro
 void mergeSort(juego* juegos, int ini, int fin){
 	// Caso base, se tiene solo 1 elemento.
 	if(ini==fin){
