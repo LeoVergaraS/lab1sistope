223dfaaf245daf939a80627e47ef0aec37ab2c61
ianRickmers <ian.rickmers@usach.cl>
2022-10-03 23:06:50 -0300

    Termino todo, faltan verificaciones

--- lab1.c
+++ lab1.c
@@ -18,6 +18,7 @@ que los hijos terminen para finalmente escribir.*/
 //entrada: Argc y argv
 //salida: 0
 int main(int argc, char *argv[]){
+	//Variables iniciales
 	int i;
 	int option;
 	char nombreEntrada[255];
@@ -26,32 +27,32 @@ int main(int argc, char *argv[]){
 	float precioMinimo=0;
 	int bandera =0;
 
-	while ((option = getopt(argc, argv, ":i:o:d:p:b")) != -1){
-		switch (option){
-		case 'i':
-			strcpy(nombreEntrada, optarg);
-			break;
-		case 'o':
-			strcpy(nombreSalida, optarg);
-			break;
-		case 'd':
-			sscanf(optarg, "%d", &anioInicio);
-			break;
-		case 'p':
-			sscanf(optarg, "%f", &precioMinimo);
-			break;
-		case 'b':
-			bandera = 1;
-			break;
-		case '?':
-			printf("unknown option: %c\n", optopt);
-			break;
-		case ':':
-			printf("Missing arg for %c\n", optopt);
-			break;
+	//getopt para leer las flags
+	while ((option = getopt(argc, argv,":i:o:d:p:b")) != -1) {
+		switch (option) {
+			case 'i' :
+				strcpy(nombreEntrada,optarg);
+				break;
+			case 'o' :
+				strcpy(nombreSalida,optarg);
+				break;
+			case 'd' :
+				anioInicio = atoi(optarg);
+				break;
+			case 'p' :
+				precioMinimo = atof(optarg);
+				break;
+			case 'b' :
+				bandera = 1;
+				break;
+			case '?' :
+				printf("Flag no reconocida\n");
+				return 0;
+			case ':' :
+				printf("Flag sin argumento\n");
+				return 0;
 		}
 	}
-
 	// Se lee el archivo de entrada.
 	int cantidadJuegos = 0;
 	juego *listaJuegos = listaDeJuegos(nombreEntrada, &cantidadJuegos);
@@ -63,7 +64,7 @@ int main(int argc, char *argv[]){
 	int cantidadAnios = 0;
 	char *archivoIntermedio = "intermedio.txt";
 	int *cabeceras = escribirJuego(archivoIntermedio, listaJuegos, cantidadJuegos, &cantidadAnios);
-	
+
 	// Se genera un arreglo para almacenar los pids de cada proceso generado
 	int pids[cantidadAnios];
 	// Se definen un pipes para la comunicacion entre proceso padre y sus hijos
@@ -88,42 +89,53 @@ int main(int argc, char *argv[]){
 	char buffer[255];
 	int posInicial = 0;
 	int aniosLista;
-	// char** listaLineas = malloc(sizeof(char*) * cantidadAnios);
+
+	// Comienza el for que crea los procesos hijos con fork()
 	for (i = 0; i < cantidadAnios; i++){
 		pids[i] = fork();
 		if (pids[i] == -1){
 			printf("Error en la creacion del proceso\n");
 			return 0;
 		}else if (pids[i] == 0){
-			//for(int k=0; k<cantidadAnios; k++){
-				close(pipesPH[i][WRITE]);
-				close(pipesHP[i][READ]);
-			
-			// Proceso hijo
+
+			// Codigo del hijo, primero se cierran los pipes
+			close(pipesPH[i][WRITE]);
+			close(pipesHP[i][READ]);
 			char buffer[255];
 			int posicionInicial, posicionFinal;
-			// printf("Proceso hijo %d creado\n", i);
 
+			// Se lee el pipe de Padre Hijo, el cual contiene la posicion inicial y final de donde se tiene que colocar 
+			// el proceso en el archivo intermedio
 			if (read(pipesPH[i][READ], buffer, sizeof(char) * 255) == -1){
 				printf("Error en la lectura del pipe\n");
 				return 0;
 			}else{
+				//Buffer que envia el mensaje del hijo al padre
+				char* bufferHijo = (char*)malloc(sizeof(char) * 10000);
+				// Se separa la posición inicial y final
 				sscanf(buffer, "%d;%d", &posicionInicial, &posicionFinal);
 				int cantidadJuegosPorAnio = 0;
+				// Se lee el archivo intermedio y se guarda en un arreglo de juegos
 				juego *juegosPorAnio = listaDeJuegosHijo(posicionInicial, posicionFinal, &cantidadJuegosPorAnio);
+				// se cuentan los juegos que cumplen con el precio mínimo establecido
 				int contador = contadorJuegosPrecioMinimo(juegosPorAnio, cantidadJuegosPorAnio, precioMinimo);
-				char* bufferHijo = malloc(sizeof(char) * 5000);
+
 				//Si contador es 0, no hay juegos con precio mayor al minimo, por lo tanto se escribe solo los parámetros del año sin juegos, 
 				//ni siquiera los juegos gratis
 				if(contador==0){
 					sprintf(bufferHijo, "%d;Año: %d\nJuego más caro:\nJuego más barato:\nPromedio de precios:\nWindows: 0 Mac: 0 Linux:0 \n",
 					juegosPorAnio[0].fecha, juegosPorAnio[0].fecha);
 					strcat(bufferHijo, "Juegos gratis:\n");		
+				
 				}else{
+
+					//En otro caso se guardan todos los datos de los juegos que si cumplan el mínimo establecido
 					int caro, barato, win, mac, lin;
 					float promedio;
+					// Se calculan los datos de los juegos
 					calculosDelHijo(juegosPorAnio, cantidadJuegosPorAnio, &caro, &barato, &promedio,precioMinimo,contador);
 					promedioPorPlataforma(juegosPorAnio, cantidadJuegosPorAnio, &win, &mac, &lin,precioMinimo,contador);
+					//Se guardan los datos en el buffer
 					sprintf(bufferHijo, "%d;Año: %d\nJuego más caro: %s\nJuego más barato: %s\nPromedio de precios: %f\nWindows: %d Mac: %d Linux: %d\n",
 					juegosPorAnio[0].fecha, juegosPorAnio[0].fecha, juegosPorAnio[caro].nombre, juegosPorAnio[barato].nombre, promedio, win, mac, lin);
 					strcat(bufferHijo, "Juegos gratis:\n");
@@ -134,38 +146,56 @@ int main(int argc, char *argv[]){
         				}
 					}
 				}
-				if (write(pipesHP[i][WRITE], bufferHijo, sizeof(char) * 5000) == -1){
+				// Se escribe en el pipe de Hijo Padre el mensaje del buffer
+				if (write(pipesHP[i][WRITE], bufferHijo, sizeof(char) * 10000) == -1){
 					printf("estoy en el error\n");
 					return 0;
 				}
-			
+				// Se cierran los pipes y se libera la memoria
+				close(pipesPH[i][READ]);
+				close(pipesHP[i][WRITE]);
+				free(bufferHijo);
 				return 0;
 			}
-			// printf("Proceso hijo %d recibio mensaje\n", i);
 		}
 	}
 
 	// Proceso padre
-	char bufferTotal[5000];
+	char bufferTotal[10000];
 	
+	// Se cierran los pipes y se escriben las posicion inicial y final para enviárselas al hijo
 	for(int i=0;i<cantidadAnios;i++){
 		close(pipesPH[i][READ]);
 		close(pipesHP[i][WRITE]);
 	sprintf(buffer, "%d;%d", posInicial, cabeceras[i]);
 	posInicial = cabeceras[i];
+
+	//Se envía el mensaje al hijo
 	if (write(pipesPH[i][WRITE], buffer, sizeof(char) * 255) == -1){
 		return 0;
 	}
 	}
+
+	//Se reinicia el archivo intermedio
 	FILE* archivoSalida = fopen(nombreSalida, "w");
+	if (archivoSalida == NULL)
+    {
+        printf("No se pudo abrir el archivo de salida\n");
+        return 0;
+    }
 	fclose(archivoSalida);
+
+	//Se lee el mensaje del hijo y se escribe en el archivo intermedio
 	for (i = 0; i < cantidadAnios; i++){
+		//Se espera a que terminen los hijos
 		waitpid(pids[i], NULL, 0);
-		if (read(pipesHP[i][READ], bufferTotal, sizeof(char) * 5000) == -1){
+		//Se lee el mensaje
+		if (read(pipesHP[i][READ], bufferTotal, sizeof(char) * 10000) == -1){
 			printf("estoy en el error\n");
 			printf("Error en la lectura del pipe\n");
 			return 0;
 		}else{
+			//Se escribe en el archivo final
 			char *token = strtok(bufferTotal, ";");
 			aniosLista = atoi(token);
 			
@@ -178,6 +208,13 @@ int main(int argc, char *argv[]){
 			}
 		}
 	}
-free(listaJuegos);
-return 0;
+	//Se libera la memoria
+	free(listaJuegos);
+	free(cabeceras);
+	//Se cierran los pipes
+	for(int i=0;i<cantidadAnios;i++){
+		close(pipesPH[i][WRITE]);
+		close(pipesHP[i][READ]);
+	}
+	return 0;
 }
